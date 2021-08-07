224cafe2401d57cb97f6c0cb0993d62647b66ce9
infodo <hoferdominik.02@gmail.com>
2021-08-07 07:52:57 +0200

    added the router

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Home</div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
