d2273e1f633699b2aad00feded7b7e39246b6c3a
infodo <hoferdominik.02@gmail.com>
2021-08-07 11:21:27 +0200

    designed the detail and setting view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
+import Cam from "../views/Cam.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/cam/:id",
+    name: "Cam",
+    component: Cam,
   },
   
 ];
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>Home</div>
+  <div>
+    
+   <div class="flex justify-center"> <h1 class="flex text-center text-5xl font-bold mt-10 mb-4">
+      WHAT THE P<div class="animate-bounce">i</div>C
+    </h1></div>
+
+    <p class="text-center text-gray-500 mb-12">Klick a camaera to see more information</p>
+
+    <card :id="1" :status="0" ></card>
+    <card :id="2" :status="3" ></card>
+    <card :id="30" :status="4" ></card>
+
+
+  </div>
 </template>
 
 <script>
+import Card from '../components/Listcard.vue'
 export default {
-  name: "Test",
+  components:{
+    Card
+  },
+  name: "List",
   created() {},
   data() {
     return {};
